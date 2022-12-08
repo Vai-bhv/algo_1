22879ab9330b60f2160b9f5d38613e2dda06212d
Vai-bhv <swciup13@gmail.com>
2022-12-08 16:24:04 +0100

    init

--- merge.py
+++ merge.py
@@ -1,73 +1,61 @@
 import sys
-
-
-if sys.argv[1] == '-i':
-    inp_1 = sys.argv[2]
-    inp_2 = sys.argv[3]
-    out_1 = sys.argv[4]
-else:
-    inp_1 = sys.argv[1]
-    inp_2 = sys.argv[2]
-    out_1 = sys.argv[3]
-
-f1 = open(inp_1)
-f2 = open(inp_2)
-f3 = open(out_1, "w")
-
-    
-
-def merge_sort(f1,f2,f3):
-    char_1 = f1.readline()
-    char_2 = f2.readline()
-
-
-    if sys.argv[1] == "-i":
-        char_1.lower()
-        char_2.lower()
-        while char_1 or char_2 != '' :
-            if char_1 == '' and char_2 != '': 
-                f3.write(char_2)
-                #print(char_2)
-                char_2 = f2.readline()
-            elif char_2 == '' and char_1 != '': 
-                f3.write(char_1)
-                #print(char_1)
-                char_1 = f1.readline()
-            elif char_1 < char_2:
-                #cur1 = f1.readline()
-                #print('List one',char_1)
-                f3.write(char_1)
-                char_1 = f1.readline() 
-            else:
-                #cur2 = f2.readline()
-                #print('List two',char_2)
-                f3.write(char_2)
-                char_2 = f2.readline()
-        
-
-
-
-    # while f1.readline() and f2.readline() != '':
-    while char_1 or char_2 != '':
-        if char_1 == '' and char_2 != '': 
-            f3.write(char_2)
-            #print(char_2)
-            char_2 = f2.readline()
-        elif char_2 == '' and char_1 != '': 
-            f3.write(char_1)
-            #print(char_1)
-            char_1 = f1.readline()
-        elif char_1 < char_2:
-            #cur1 = f1.readline()
-            #print('List one',char_1)
-            f3.write(char_1)
-            char_1 = f1.readline() 
+if len(sys.argv) == 5:
+    file1 = open(str(sys.argv[2]),'r')
+    file2 = open(str(sys.argv[3]),'r')
+    l1 = file1.readline()
+    l2 = file2.readline()
+    merged_file = open(str(sys.argv[4]),'a')
+    while True:
+        if not l1 and not l2:
+            break
+        elif l1 and not l2:
+            merged_file.write(l1)
+            l1=file1.readline()
+        elif l2 and not l1:
+            merged_file.write(l2)
+            l2=file2.readline()
+        elif l1.endswith(':'):
+            merged_file.write(l1)
+            l1=file1.readline()
+        elif l2.endswith(':'):
+            merged_file.write(l2)
+            l2=file2.readline()
         else:
-            #cur2 = f2.readline()
-            #print('List two',char_2)
-            f3.write(char_2)
-            char_2 = f2.readline()
-    f3.close()
-merge_sort(f1,f2,f3)
-
-
+            lineC1 = l1.lower()
+            lineC2 = l2.lower()
+            if lineC1 < lineC2:
+                merged_file.write(l1)
+                l1=file1.readline()
+            if lineC1 > lineC2:
+                merged_file.write(l2)
+                l2=file2.readline()
+    merged_file.close()
+else:
+    file1 = open(str(sys.argv[1]), 'r')
+    file2 = open(str(sys.argv[2]), 'r')
+    l1 = file1.readline()
+    l2 = file2.readline()
+    merged_file = open(str(sys.argv[3]), 'a')
+    while True:
+        if not l1 and not l2:
+            break
+        elif l1 and not l2:
+            merged_file.write(l1)
+            l1 = file1.readline()
+        elif l2 and not l1:
+            merged_file.write(l2)
+            l2 = file2.readline()
+        elif l1.endswith(':'):
+            merged_file.write(l1)
+            l1=file1.readline()
+        elif l2.endswith(':'):
+            merged_file.write(l2)
+            l2=file2.readline()
+        else:
+            if l1 < l2:
+                merged_file.write(l1)
+                l1 = file1.readline()
+            if l1 > l2:
+                merged_file.write(l2)
+                l2 = file2.readline()
+    merged_file.close()
